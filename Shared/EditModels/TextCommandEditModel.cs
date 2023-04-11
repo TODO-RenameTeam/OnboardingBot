de8118829bf2b4be300577c51578c88a544b0c93
Nikita Shidlovksy <nik-801@yandex.ru>
2023-04-11 17:07:47 +0300

    Fixing.

--- Shared/EditModels/TextCommandEditModel.cs
+++ Shared/EditModels/TextCommandEditModel.cs
@@ -1,13 +1,17 @@
+using System.ComponentModel.DataAnnotations;
 using OnboardingBot.Shared.ViewModels;
 
 namespace OnboardingBot.Shared.EditModels;
 
 public class TextCommandEditModel
 {
+    [Required]
     public string Name { get; set; }
 
+    [Required]
     public string Template { get; set; }
 
+    [Required]
     public string Text { get; set; } = String.Empty;
 
     public HashSet<string>? Images { get; set; } = new();
